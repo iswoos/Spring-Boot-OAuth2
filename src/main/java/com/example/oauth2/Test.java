3f63d28f4beb04d04f5f7490b6d47d7956e8bf39
seungwoo-lee <vugil@naver.com>
2023-10-24 23:49:12 +0900

    Begin : localDB 연결 및 Oauth 설정

--- src/main/java/com/example/oauth2/Test.java
+++ src/main/java/com/example/oauth2/Test.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Test {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public String test() {
         return "오랜만이야!";
     }
