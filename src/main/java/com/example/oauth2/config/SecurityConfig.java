0099bfe3b80de32a49e8a276a3de87516ea7d93a
seungwoo-lee <vugil@naver.com>
2023-10-31 23:35:13 +0900

    Comment : 설명 주석추가

--- src/main/java/com/example/oauth2/config/SecurityConfig.java
+++ src/main/java/com/example/oauth2/config/SecurityConfig.java
@@ -9,10 +9,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.SecurityFilterChain;
 
+
+/**
+ * Form Login에 사용하는 Security Config
+ */
+/* Security Config2(Jwt Token Login에서 사용)와 같이 사용하면 에러 발생
+Security Form Login 진행하기 위해서는 이 부분 주석 제거 후 Security Config2에 주석 추가*/
 @Configuration
+@EnableWebSecurity
+
+/* 다른 인증, 인가 방식 적용을 위한 어노테이션
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+*/
 @RequiredArgsConstructor
 public class SecurityConfig {
 
